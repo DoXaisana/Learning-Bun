25cb6d3d16c5c1b13250d8d77af055428ce5b907
unknown <dodoxaisana123@gmail.com>
2024-12-24 00:19:30 +0700

    upload app folder in elysia

---
+++ elysia/app/src/index.ts
@@ -0,0 +1,8 @@
+import { Elysia } from 'elysia'
+
+new Elysia()
+    .get('/', 'Hello Elysia')
+    .get('/user/:id', ({ params: { id }}) => id)
+    .post('/form', ({ body }) => body)
+    .listen(3000)
+  
